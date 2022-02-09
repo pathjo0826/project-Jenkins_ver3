fc9a10a23068f248cd9a3d99ed05e12a3c9c0cec
patrikHjortberg <pathjo0826@skola.goteborg.se>
2022-02-09 17:55:07 +0100

    added test for FillMap class

---
+++ src/test/java/se/yrgo/classes/TestFillMap.java
@@ -0,0 +1,21 @@
+package se.yrgo.classes;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+public class TestFillMap {
+
+    @Test
+    public void testFillMap(){
+
+        HashMap<String, String> hash = new HashMap<>();
+        FillMap fm = new FillMap();
+
+        assertFalse(fm.fillMap(hash).isEmpty());            // Tests that hashMap is loaded (or at least not empty)
+                                                            // at start of running the program.
+    }
+}
