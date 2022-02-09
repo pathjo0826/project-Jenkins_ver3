f35c17a67ef072c8ff2d6cad19112649e30be996
Marelw <marelw1112@skola.goteborg.se>
2022-02-09 14:17:52 +0100

    Rework on TestAddWords file

--- src/test/java/se/yrgo/classes/TestAddWords.java
+++ src/test/java/se/yrgo/classes/TestAddWords.java
@@ -3,26 +3,24 @@ package se.yrgo.classes;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 
 
-public class AddWordsTest {
+public class TestAddWords {
     private String userEntersWords;
     private String shortWord;
+    private HashMap<String, String> storedWords;
 
-
-    @Test
-    public void TestAddingWordsToHashMap() {
-
-        HashMap<String, String> storedWords = new HashMap<>();
-
+    public void addWordsTest() {
+        storedWords = new HashMap<>();
         shortWord = "lol";
         userEntersWords = "laugh out loud";
-
         storedWords.put(shortWord, userEntersWords);
+    }
 
+    @Test
+    public void testAddingWordsToHashMap() {
         assertEquals(storedWords.put(shortWord, userEntersWords), storedWords.get(shortWord), "Testing to add one word and then fetching the key from the map");
     }
 
