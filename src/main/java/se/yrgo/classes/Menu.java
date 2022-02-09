24cae3eb0dc8ff9bac360f62bc98f0ab899bd66f
Robert <robmoi0112@skola.goteborg.se>
2022-02-09 09:04:57 +0100

    adding while loop to menu

--- src/main/java/se/yrgo/classes/Menu.java
+++ src/main/java/se/yrgo/classes/Menu.java
@@ -1,37 +1,43 @@
 package se.yrgo.classes;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
 
-    public void programMenu() {
+    public void programMenu() throws IOException {
 
-        HashMap <String, String> map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         WordFinder wf = new WordFinder();
+        AddWords aw = new AddWords();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Meny: \n Press 1 to add internet abbreviation \n Press 2 to look up internet abbreviation");
+        System.out.println("Meny: \n Press 1 to add internet abbreviation \n Press 2 to look up internet abbreviation \n Press 3 to exit");
         int input = 0;
 
         try {
             input = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
+        while (input != 3) {
 
-        switch (input) {
+            switch (input) {
 
-            case 1:
-
-                break;
-
-            case 2:
-                System.out.println("Enter abbreviation to look up: ");
-                String abbreviation = scanner.nextLine();
-                wf.findExpression(map, abbreviation);
-                break;
+                case 1:
+                    aw.AddWord();
+                    break;
+                case 2:
+                    System.out.println("Enter abbreviation to look up: ");
+                    String abbreviation = scanner.nextLine();
+                    System.out.println(wf.findExpression(map, abbreviation)); 
+                    break;
+            }
+            System.out.println("Meny: \n Press 1 to add internet abbreviation \n Press 2 to look up internet abbreviation");
+            input = scanner.nextInt();
         }
+        System.out.println("Welcome back!");
 
     }
 }
