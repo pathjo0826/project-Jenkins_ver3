dc6ef014cd149fb4f7964fbb8d9eea213b7fefbf
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-02-12 12:38:47 +0100

    added method description

--- src/main/java/se/yrgo/classes/EnterExpressionUI.java
+++ src/main/java/se/yrgo/classes/EnterExpressionUI.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class EnterExpressionUI {
 
+    /**
+     * This method handles user input in form of a String. 
+     * It returns the user input in form of a String.
+     * @param scanner
+     * @return
+     */
 
     public String enterExpression(Scanner scanner) {
         System.out.println("Enter expression to look up: ");
